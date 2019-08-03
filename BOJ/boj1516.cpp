4d9e850a52e39ac5ccb97704d0d343a660bb349c
doorisopen <audauddl2@gmail.com>
2019-08-04 03:05:57 +0900

    .

---
+++ BOJ/boj1516.cpp
@@ -0,0 +1,55 @@
+// 1516 - 게임 개발
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <queue>
+#define MAX 501
+
+using namespace std;
+
+int n, inDegree[MAX], time[MAX], result[MAX];
+vector<int> a[MAX];
+
+void topologySort(){
+	queue<int> q;
+	// 진입 차수가 0인 노드를 큐에 삽입한다.
+	for(int i = 1; i <= n; i++) {
+		if(inDegree[i] == 0) {
+			result[i] = time[i];
+			q.push(i);
+		}
+	} 
+	// 정렬이 완전히 수행되려면 정확히 n개의 노드를 방문한다.
+	for(int i = 1; i <= n; i++) {
+		int x = q.front();
+		q.pop();
+		for(int i = 0; i < a[x].size(); i++) {
+			int y = a[x][i];
+			result[y] = max(result[y], result[x] + time[y]);
+			// 새롭게 진입차수가 0 이된 정점을 큐에 삽입한다. 
+			if(--inDegree[y] == 0) {
+				q.push(y);
+			}
+		}
+	}
+	for(int i = 1; i <= n; i++) {
+		printf("%d\n", result[i]);
+	}
+}
+
+int main(void) {
+	scanf("%d",&n);
+	for(int i = 1; i <= n; i++)
+	{
+		scanf("%d", &time[i]);
+		while(1){
+			int x;
+			scanf("%d", &x);
+			if(x == -1) break;
+			inDegree[i]++;	
+			a[x].push_back(i);		
+		}
+	}
+	topologySort();
+	return 0;
+}
